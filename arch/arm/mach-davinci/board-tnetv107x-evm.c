1e64ebf8c4a67979f883cda01cbe126be7454fd6
Cyril Chemparathy <cyril@ti.com>
2010-10-19 06:46:03 -0400

    davinci: Add platform resource structs for tnetv107x musb

--- arch/arm/mach-davinci/board-tnetv107x-evm.c
+++ arch/arm/mach-davinci/board-tnetv107x-evm.c
@@ -490,6 +490,7 @@ static int __init tnetv107x_evm_late_init(void)
 {
 	platform_device_register(&backlight_device);
 	platform_device_register(&i2c_device);
+	tnetv107x_register_usb20();
 	return 0;
 }
 late_initcall(tnetv107x_evm_late_init);
